ed082af9b81a668599b3c0ed943e9c1df0f24ff1
vitoraraujodev <vitoraraujo.dev@gmail.com>
2019-11-19 18:38:05 -0300

    Eslização do Carrinho

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,11 +1,68 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
+
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
   return (
-    <div>
-      <h1>Carrinho</h1>
-    </div>
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://static.netshoes.com.br/produtos/tenis-asics-gel-equation-9-masculino/38/D18-0650-038/D18-0650-038_detalhe1.jpg?resize=280:280"
+                alt="Tênis"
+              />
+            </td>
+            <td>
+              <strong>Tênis legal</strong>
+              <span>R$129,90</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7219c1" />
+                </button>
+                <input type="number" readOnly value={2} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7219c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$259,80</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7151c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+
+      <footer>
+        <button type="button">Finalizar pedido</button>
+
+        <Total>
+          <span>TOTAL</span>
+          <strong>R$259,80</strong>
+        </Total>
+      </footer>
+    </Container>
   );
 }
